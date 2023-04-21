1cc78f76f3e87ad009cf330b25114f8ead05fc5c
Alyss Weissglass <alyss@benchling.com>
2023-04-20 22:52:30 -0500

    Lint with Black and isort (#3)
    Added linting and instructions on how to install a pre-push hook.

--- Person.py
+++ Person.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-import random_address
-import names
-import secrets
 import csv
+import secrets
 from random import randint
+
+import names
+import random_address
 
 from WordBank import WordBank
 
 states = ["MO", "KS", "NE", "IA", "WI"]
+
 
 def make_email(first_name, last_name):
     email_endings = ["@hotmail.com", "@yahoo.com", "@gmail.com", "@aol.com"]
@@ -15,41 +17,48 @@ def make_email(first_name, last_name):
 
     assembled_email = ""
 
-    pick_email_method = randint(0,4)
-    if (pick_email_method == 0):
+    pick_email_method = randint(0, 4)
+    if pick_email_method == 0:
         assembled_email = first_name[0] + last_name + email_end
-    elif (pick_email_method == 1):
+    elif pick_email_method == 1:
         assembled_email = first_name + "." + last_name + email_end
-    elif (pick_email_method == 2):
+    elif pick_email_method == 2:
         assembled_email = first_name + last_name + email_end
     else:
-        assembled_email = first_name + last_name + str(randint(0,9)) + email_end
+        assembled_email = first_name + last_name + str(randint(0, 9)) + email_end
 
     return assembled_email
 
+
 def make_phone_number(state="MO"):
     area_codes = {
-        "MO" : ["314", "557", "417", "573", "636", "660", "816"],
-        "KS" : ["316", "620", "785", "913"],
-        "NE" : ["308", "402", "531"],
-        "IA" : ["319", "515", "563", "641", "712"],
-        "WI" : ["262", "274", "414", "353", "608", "715", "534", "920"]
+        "MO": ["314", "557", "417", "573", "636", "660", "816"],
+        "KS": ["316", "620", "785", "913"],
+        "NE": ["308", "402", "531"],
+        "IA": ["319", "515", "563", "641", "712"],
+        "WI": ["262", "274", "414", "353", "608", "715", "534", "920"],
     }
 
     phone_number = secrets.choice(area_codes[state])
 
     for i in range(7):
-        phone_number += str(randint(0,9))
+        phone_number += str(randint(0, 9))
 
     return phone_number
 
-missouri_data = list(csv.DictReader(open('data_sets/Missouri_Zip_Codes_by_County_City.csv')))
+
+missouri_data = list(
+    csv.DictReader(open("data_sets/Missouri_Zip_Codes_by_County_City.csv"))
+)
+
 
 class Person:
     def __init__(self, role=None):
         self.first_name = names.get_first_name()
         self.last_name = names.get_last_name()
-        self.address = {"street": random_address.real_random_address()["address1"]} | secrets.choice(missouri_data)
+        self.address = {
+            "street": random_address.real_random_address()["address1"]
+        } | secrets.choice(missouri_data)
         self.email = make_email(self.first_name, self.last_name)
         self.phone_number = make_phone_number(secrets.choice(states))
         self.role = WordBank().get_person("role") if role != None else None
--- WordBank.py
+++ WordBank.py
@@ -1,18 +1,29 @@
-
 #!/usr/bin/env python3
-from data_sets import mo_schools, mo_hospitals, states_of_denial, ncte_mo_state_report,
-from Person import Person
-from secrets import choice
-from random import sample
 import sys
+from random import sample
+from secrets import choice
+
+from data_sets import mo_hospitals, mo_schools, ncte_mo_state_report, states_of_denial
+from Person import Person
 
 categories = ["medical", "school"]
 
 medical_verbs = ["prescribed", "is prescribing", "gave", "is giving"]
-medical_things = ["hormones", "estrogen", "estrogen patches", "estrodol", "testosterone", "T", "estrogen shots", "testosterone shots", "testosterone jelly"]
+medical_things = [
+    "hormones",
+    "estrogen",
+    "estrogen patches",
+    "estrodol",
+    "testosterone",
+    "T",
+    "estrogen shots",
+    "testosterone shots",
+    "testosterone jelly",
+]
 
-locations = {"medical" : ["practice", "hospital", "office", "clinic"],
-             "school": ["school", "class", "school system", "high school"]
+locations = {
+    "medical": ["practice", "hospital", "office", "clinic"],
+    "school": ["school", "class", "school system", "high school"],
 }
 for category in locations:
     for location in category:
@@ -20,15 +31,95 @@ for category in locations:
         locations[category].append(f"{location} ({choice(data_set)})")
 
 
-communication_verbs = ["told", "confessed", 'said', 'informed', "reported", "mentioned"]
+communication_verbs = ["told", "confessed", "said", "informed", "reported", "mentioned"]
 
-school_classes = ["math", "science", "biology", "physics", "chemistry", "earth science", "environmental science", "zoology", "botany", "anatomy", "physiology", "genetics", "ecology", "meteorology", "oceanography", "geology", "astronomy", "statistics", "calculus", "algebra", "geometry", "trigonometry", "differential equations", "economics", "macroeconomics", "microeconomics", "history", "geography", "world history", "US history", "government", "political science", "psychology", "sociology", "anthropology", "philosophy", "English literature", "creative writing", "French", "linguistics", "computer science", "PE"]
+school_classes = [
+    "math",
+    "science",
+    "biology",
+    "physics",
+    "chemistry",
+    "earth science",
+    "environmental science",
+    "zoology",
+    "botany",
+    "anatomy",
+    "physiology",
+    "genetics",
+    "ecology",
+    "meteorology",
+    "oceanography",
+    "geology",
+    "astronomy",
+    "statistics",
+    "calculus",
+    "algebra",
+    "geometry",
+    "trigonometry",
+    "differential equations",
+    "economics",
+    "macroeconomics",
+    "microeconomics",
+    "history",
+    "geography",
+    "world history",
+    "US history",
+    "government",
+    "political science",
+    "psychology",
+    "sociology",
+    "anthropology",
+    "philosophy",
+    "English literature",
+    "creative writing",
+    "French",
+    "linguistics",
+    "computer science",
+    "PE",
+]
 
-sports = ["bobsledding", "synchronized swimming", "dragon boat racing", "water polo", "mountain biking", "snowboarding", "archery", "wingsuit flying", "ice climbing", "extreme ironing", "yachting", "sailing", "powerlifting", "strongman", "dodgeball", "spearfishing", "wakeboarding", "kitesurfing", "flyboarding", "parkour", "rock climbing", "obstacle course racing", "roller derby", "underwater hockey", "sled dog racing"]
+sports = [
+    "bobsledding",
+    "synchronized swimming",
+    "dragon boat racing",
+    "water polo",
+    "mountain biking",
+    "snowboarding",
+    "archery",
+    "wingsuit flying",
+    "ice climbing",
+    "extreme ironing",
+    "yachting",
+    "sailing",
+    "powerlifting",
+    "strongman",
+    "dodgeball",
+    "spearfishing",
+    "wakeboarding",
+    "kitesurfing",
+    "flyboarding",
+    "parkour",
+    "rock climbing",
+    "obstacle course racing",
+    "roller derby",
+    "underwater hockey",
+    "sled dog racing",
+]
 sports_descriptors = {
     "male": ["boy's", "boys", "boys'", "men's", "mens", "guys', 'guys's, 'guys'"],
-    "female": ["girls'", "girl's", "girls", "women's", "womens", "ladies", "ladys", "ladies'", "ladies's"],
-    "generic": ["competitive", "varsity", "junior varsity"]}
+    "female": [
+        "girls'",
+        "girl's",
+        "girls",
+        "women's",
+        "womens",
+        "ladies",
+        "ladys",
+        "ladies'",
+        "ladies's",
+    ],
+    "generic": ["competitive", "varsity", "junior varsity"],
+}
 sport_action_verbs = ["play", "compete", "participate"]
 
 for school_class in sample(list(school_classes), 5):
@@ -38,11 +129,32 @@ for school_class in sample(list(school_classes), 5):
 words_for_guys = ["boy", "young man", "guy"]
 words_for_girls = ["girl", "young lady", "lady", "young woman"]
 
-mood_words = ["worried", "excited", "afraid", "angry", "concerned", "ecstatic", "devastated", "furious", "terrified", "elated", "enraged", "overjoyed", "heartbroken", "livid", "petrified", "thrilled", "indignant", "horrified", "exhilarated", "despairing", "incensed"]
+mood_words = [
+    "worried",
+    "excited",
+    "afraid",
+    "angry",
+    "concerned",
+    "ecstatic",
+    "devastated",
+    "furious",
+    "terrified",
+    "elated",
+    "enraged",
+    "overjoyed",
+    "heartbroken",
+    "livid",
+    "petrified",
+    "thrilled",
+    "indignant",
+    "horrified",
+    "exhilarated",
+    "despairing",
+    "incensed",
+]
 
 
-
-class WordBank :
+class WordBank:
     def __init__(self):
         complainer = Person()
         child = Person("child")
@@ -57,31 +169,130 @@ class WordBank :
         guy = choice(words_for_guys)
         girl = choice(words_for_girls)
         sport_action_verb = choice(sport_action_verbs)
-    
+
     opinion_words = ["against", "in support of"]
-    
-    persons = { 
-            "medical": ["patient", "doctor", "physician", "nurse practitioner", "medical provider", "GP", "PCP"],
-            "school": ["student", "teacher", "administrator", "principal", "teaching aid", "school nurse"],
-            "child": ["son", "daughter", "kid", "child", "nephew", "niece", "nibling", "granddaughter", "grandchild", "grandson"],
-            "community": ["neighbor", "pastor", "teacher", "coach", "cousin", "aunt", "uncle", "babysitter", "dentist", "doctor", "barber", "hairdresser", "tutor", "music teacher", "dance instructor", "coworker", "boss"]}
+
+    persons = {
+        "medical": [
+            "patient",
+            "doctor",
+            "physician",
+            "nurse practitioner",
+            "medical provider",
+            "GP",
+            "PCP",
+        ],
+        "school": [
+            "student",
+            "teacher",
+            "administrator",
+            "principal",
+            "teaching aid",
+            "school nurse",
+        ],
+        "child": [
+            "son",
+            "daughter",
+            "kid",
+            "child",
+            "nephew",
+            "niece",
+            "nibling",
+            "granddaughter",
+            "grandchild",
+            "grandson",
+        ],
+        "community": [
+            "neighbor",
+            "pastor",
+            "teacher",
+            "coach",
+            "cousin",
+            "aunt",
+            "uncle",
+            "babysitter",
+            "dentist",
+            "doctor",
+            "barber",
+            "hairdresser",
+            "tutor",
+            "music teacher",
+            "dance instructor",
+            "coworker",
+            "boss",
+        ],
+    }
     for category in persons:
         for person in category:
-            persons[category].append(f"the {person} of my {choice(persons['community'])}")
+            persons[category].append(
+                f"the {person} of my {choice(persons['community'])}"
+            )
             persons[category].append(f"my {choice(choice(persons))}'s {person}")
-    
+
     locations = {
-        "medical" : ["practice", "hospital", "office", "clinic"],
-        "school": ["school", "class", "school system", "high school"]
+        "medical": ["practice", "hospital", "office", "clinic"],
+        "school": ["school", "class", "school system", "high school"],
     }
 
     verbs = {
-        "action": ["skydive", "scuba dive", "bungee jump", "learn to surf", "climb a mountain", "go zorbing", "take a trapeze class", "take a survival course", "do acroyoga", "learn archery", "take a fencing class", "try parkour", "learn circus arts", "go horseback riding", "learn to juggle", "try paddleboarding", "do tai chi", "take a yoga class", "try aerial silks", "watch TV shows", "play video games", "read comics", "build a sandcastle", "knit a scarf", "write love letters", "try new foods", "explore a city", "attend concerts", "play board games", "watch movies", "write poetry", "attend a stand-up comedy show", "solve puzzles", "paint by numbers", "listen to music", "build with Lego bricks", "collect stamps", "play with slime", "make balloon animals", "participate in a flash mob", "collect rocks", "build the Statue of Liberty with lincoln logs"], 
-        "communication": ["told", "confessed to", 'said', 'informed', "reported to", "mentioned to", "shared with"]}
-    
+        "action": [
+            "skydive",
+            "scuba dive",
+            "bungee jump",
+            "learn to surf",
+            "climb a mountain",
+            "go zorbing",
+            "take a trapeze class",
+            "take a survival course",
+            "do acroyoga",
+            "learn archery",
+            "take a fencing class",
+            "try parkour",
+            "learn circus arts",
+            "go horseback riding",
+            "learn to juggle",
+            "try paddleboarding",
+            "do tai chi",
+            "take a yoga class",
+            "try aerial silks",
+            "watch TV shows",
+            "play video games",
+            "read comics",
+            "build a sandcastle",
+            "knit a scarf",
+            "write love letters",
+            "try new foods",
+            "explore a city",
+            "attend concerts",
+            "play board games",
+            "watch movies",
+            "write poetry",
+            "attend a stand-up comedy show",
+            "solve puzzles",
+            "paint by numbers",
+            "listen to music",
+            "build with Lego bricks",
+            "collect stamps",
+            "play with slime",
+            "make balloon animals",
+            "participate in a flash mob",
+            "collect rocks",
+            "build the Statue of Liberty with lincoln logs",
+        ],
+        "communication": [
+            "told",
+            "confessed to",
+            "said",
+            "informed",
+            "reported to",
+            "mentioned to",
+            "shared with",
+        ],
+    }
+
     def get_mood_word():
         return choice(mood_words)
-    
+
     def get_sports_team(gender=choice(["male", "female"]), sport=choice(sports)):
         gendered_adj = choice(sports_descriptors[gender])
         adj = choice(f" {sports_descriptors['generic']} " + [" "])
@@ -90,12 +301,12 @@ class WordBank :
 
     def get_person(self, category):
         return choice(self.persons[category])
-    
+
     def get_location(self, category):
         return choice(locations[category])
-    
+
     def get_verb(self, type_of_verb):
         return choice(self.verbs[type_of_verb])
-    
+
     def get_stance(self):
         return choice(self.opinion_words)
